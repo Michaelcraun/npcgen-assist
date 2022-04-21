f9feefbcede734c6ee4ad0e09b402286e3b71493
Michael Craun <michael.craun@gmail.com>
2022-04-21 06:43:12 -0400

    Initial Commit

---
+++ NPCGen Assist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NPCGen Assist
+//
+//  Created by Michael Craun on 4/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NPCGen Assist/NPCGen_AssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  NPCGen_AssistApp.swift
+//  NPCGen Assist
+//
+//  Created by Michael Craun on 4/21/22.
+//
+
+import SwiftUI
+
+@main
+struct NPCGen_AssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
